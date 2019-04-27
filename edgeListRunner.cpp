de189c3b3a953ee66597f4802a19f193d25760f8
Keat's Mac <keaton.j.whitehead@gmail.com>
2019-04-26 19:04:16 -0600

    Adding test file and a runner file

---
+++ edgeListRunner.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+int main() {
+     std::cout <<
+    "Hello World!";
+  std::cout
+    << "I'm a C++ program";
+}
