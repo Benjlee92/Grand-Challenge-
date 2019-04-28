48106c7bce1cfe157642b9c9f1f6472ba808ca44
Keat's Mac <keaton.j.whitehead@gmail.com>
2019-04-27 20:13:01 -0600

    inital code to read in a given text file for the edgeListRunner code

--- edgeListRunner.cpp
+++ edgeListRunner.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
-#include <iomanip>
 #include <fstream>
 using namespace std;
-
 int main() {
-     std::cout <<
-    "Hello World!";
-  std::cout
-    << "I'm a C++ program";
+
+ ifstream myReadFile;
+ myReadFile.open("testcount.txt");
+ char output[100];
+ cout << "Test2" << endl;
+ if (myReadFile.is_open()) {
+      cout << "File opened" << endl;
+ while (!myReadFile.eof()) {
+    myReadFile >> output;
+    cout<<output;
+ }
+}
+myReadFile.close();
+return 0;
 }
