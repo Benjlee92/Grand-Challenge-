739d19fd0b9640ccf6fd1857e8b629726ee22c69
Zazu6141 <Zazu6141@colorado.edu>
2019-05-02 19:49:32 -0600

    Update on GraphNodes

--- ZacksStuff/GCGraph.cpp
+++ ZacksStuff/GCGraph.cpp
@@ -4,3 +4,12 @@
 #include <fstream>
 
 using namespace std;
+
+
+void GCGraph::buildVertexList();
+void GCGraph::addAdjVertex(vertex );
+void GCGraph::addThree();
+void GCGraph::ThreeLineAdd();
+void GCGraph::TriangleAdd();
+void GCGraph::addSingleEdge();
+void GCGraph::addMultipleEdges();
